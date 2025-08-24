ae20880885c9db02125bc16d65b969572c6aaf7f
manohar6363 <lakshmimanohart@gmail.com>
2025-08-24 20:35:44 +0530

    Updated entrypoint.sh to avoid duplicate superuser error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-
-# Exit immediately if a command fails
 set -e
 
-# Run Django migrations
 echo "Running migrations..."
 python manage.py migrate
 
-# Create a superuser if not exists
 echo "Creating superuser if not exists..."
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('manoharreddy', 'lakshmimanohart@gmail.com', 'reddy')
+if not User.objects.filter(username='manoharreddy').exists():
+    User.objects.create_superuser('syko_reddy', 'lakshmimanohart@gmail.com', 'reddy2005')
+else:
+    print('Superuser already exists.')
 "
 
-# Start the server
 echo "Starting server..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
